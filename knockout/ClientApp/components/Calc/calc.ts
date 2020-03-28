3951157d85204408f6e5536f488772e3ce5d292f
T33mac <31869841+T33mac@users.noreply.github.com>
2020-03-27 18:08:43 -0700

    start ko

---
+++ knockout/ClientApp/components/Calc/calc.ts
@@ -0,0 +1,17 @@
+﻿import * as ko from 'knockout';
+
+class CounterExampleViewModel {
+    public currentCount = ko.observable(0);
+
+    public incrementCounter() {
+        let prevCount = this.currentCount();
+        this.currentCount(prevCount + 1);
+    }
+
+    public incrementCounter2() {
+        let prevCount = this.currentCount();
+        this.currentCount(prevCount - 2);
+    }
+}
+
+export default { viewModel: CounterExampleViewModel, template: require('./counter-example.html') };
